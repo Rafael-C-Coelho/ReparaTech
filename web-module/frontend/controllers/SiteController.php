9d6d517b3b96236d29316fff7d5faec13ca8d60d
Diogo Gouveia <diogo.gouveia2@gmail.com>
2024-10-28 22:24:39 +0000

    Fix of sign up

--- web-module/frontend/controllers/SiteController.php
+++ web-module/frontend/controllers/SiteController.php
@@ -154,7 +154,6 @@ class SiteController extends Controller
     {
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
             return $this->goHome();
         }
 
