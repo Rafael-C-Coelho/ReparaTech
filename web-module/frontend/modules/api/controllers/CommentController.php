55ebe46e26f2472273ec3150930cda5751158f7c
2232357 <2232357@my.ipleiria.pt>
2024-12-27 23:25:42 +0000

    Comment Requests
    Very Incomplete, controller created

---
+++ web-module/frontend/modules/api/controllers/CommentController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace frontend\modules\api\controllers;
+
+use common\models\Comment;
+use frontend\modules\api\helpers\AuthBehavior;
+use yii\data\ActiveDataProvider;
+use yii\rest\ActiveController;
+
+class CommentController extends ActiveController
+{
+    public $modelClass = 'common\models\Comment';
+    public $user = null;
+
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            'authenticator' => [
+                'class' => AuthBehavior::class,
+                'except' => ['index',
+                            'view'],
+            ]
+        ]);
+    }
+    public function actions(){
+        $actions = parent::actions();
+        unset($actions['index']);
+        unset($actions['view']);
+        unset($actions['create']);
+        unset($actions['update']);
+        unset($actions['delete']);
+        return $actions;
+    }
+
+    protected function verbs(){
+        return array_merge(parent::verbs(), [
+
+        ]);
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if(\Yii::$app->user->identity->hasRole('admin' || 'manager' || 'technician' )){
+
+            throw new \yii\web\ForbiddenHttpException("You can only view comments.");
+        }
+    }
+
+    public function actionIndex($page = 1, $perPage = 10){
+
+        $activeDataProvider = new ActiveDataProvider([
+            'query' => Comment::find(),
+            'pagination' => [
+                'defaultPageSize' => $perPage,
+                'pageSizeLimit' => [1, 100],
+                'pageParam' => 'page',
+            ],
+        ]);
+
+        return (['comments' => $activeDataProvider, 'page' => $page, 'total'=> $activeDataProvider->getTotalCount(), "status" => "success"]);
+    }
+
+    public function actionView($id){
+        $comment = Comment::findOne($id);
+        if($comment){
+            return ['comment' => $comment, 'status' => "success"];
+        } else {
+            return ['message' => "Comment not found", "status" => "error"];
+        }
+    }
+}
