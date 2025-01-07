8a0f5b7d9f53bee50c1eab358f50df2f15e71cd5
2232357 <2232357@my.ipleiria.pt>
2025-01-07 17:55:11 +0000

    Functional Registration Test done
    Registration Test done

--- web-module/frontend/tests/functional/RegistrationCest.php
+++ web-module/frontend/tests/functional/RegistrationCest.php
@@ -22,25 +22,24 @@ class RegistrationCest
         $I->seeLink('Sign up');
         $I->click('Sign up');
         $I->see('Please fill out the following fields to signup:');
-        //$I->seeInCurrentUrl('/site/signup');
+        $I->seeInCurrentUrl('/site/signup');
 
-        //$I->seeElement('#form-signup');
-        $I->see('Signup');
+        $I->seeElement('#form-signup');
         $I->click('Signup');
         $I->seeValidationError('Username cannot be blank.');
         $I->seeValidationError('Name cannot be blank.');
         $I->seeValidationError('Email cannot be blank.');
         $I->seeValidationError('Password cannot be blank.');
 
-        $I->fillField('SignupForm[username]', 'tester');
-        $I->fillField('SignupForm[name]', 'Tester');
-        $I->fillField('SignupForm[email]', 'tester@tester.test');
-        $I->fillField('SignupForm[password]', 'tester');
+        $I->fillField('#signupform-username', 'tester');
+        $I->fillField('#signupform-name', 'Tester');
+        $I->fillField('#signupform-email', 'tester@tester.test');
+        $I->fillField('#signupform-password', 'Te@232');
 
         $I->click('Signup');
         $I->seeValidationError('Password should contain at least 8 characters.');
+        $I->fillField('#signupform-password', 'Tester@123');
 
-        $I->fillField('SignupForm[password]', 'Tester@123');
         $I->click('Signup');
     }
 
