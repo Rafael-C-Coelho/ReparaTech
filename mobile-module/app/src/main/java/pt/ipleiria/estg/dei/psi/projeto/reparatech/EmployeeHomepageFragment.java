4ddac8bb7bb0efc5f007beecc56504aaf9774fa7
Rafael Reis <2231432@my.ipleiria.pt>
2025-01-18 22:09:50 +0000

    Update Homepage Employee

--- mobile-module/app/src/main/java/pt/ipleiria/estg/dei/psi/projeto/reparatech/EmployeeHomepageFragment.java
+++ mobile-module/app/src/main/java/pt/ipleiria/estg/dei/psi/projeto/reparatech/EmployeeHomepageFragment.java
@@ -15,70 +15,12 @@ import pt.ipleiria.estg.dei.psi.projeto.reparatech.models.BestSellingProduct;
 
 public class EmployeeHomepageFragment extends Fragment {
 
-    //private HorizontalScrollView hScrollViewRepairCategories;
-    //private HorizontalScrollView hScrollBestSellingProducts;
-    //private ArrayList<RepairExample> repairExamples;
-    //private ArrayList<BestSellingProduct> bestSellingProducts;
-    //private RepairExample repairExample;
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_employee_homepage, container, false);
         
-        //LinearLayout gallery = view.findViewById(R.id.RepairCategories);
-        //LinearLayout llBestSellingProducts = view.findViewById(R.id.BestSellingProducts);
 
-        //hScrollViewRepairCategories = view.findViewById(R.id.hScrollViewRepairCategories);
-        //hScrollBestSellingProducts = view.findViewById(R.id.hScrollBestSellingProducts);
-        
-
-        // repairExamples = ReparaTechSingleton.getInstance(getContext()).getRepairExamples();
-        //bestSellingProducts = ReparaTechSingleton.getInstance(getContext()).getbestSellingProductsExamples();
-        /*
-        for (RepairExample repairExample: repairExamples) {
-            View cardView = inflater.inflate(R.layout.item_repaircategorie_homepage,gallery,false);
-
-            ImageView imgCapa = cardView.findViewById(R.id.imgCapa);
-            TextView tvBrokenScreen = cardView.findViewById(R.id.tvBrokenScreen);
-            imgCapa.setImageResource(repairExample.getImg());
-            tvBrokenScreen.setText(repairExample.getTitle());
-
-
-            gallery.addView(cardView);
-
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (repairExample.getId() == -1) {
-                        Intent intent = new Intent(getActivity(), RepairCategoriesListActivity.class);
-                        startActivity(intent);
-                    }
-                }
-            });
-        }
-        */
-
-
-        /*
-        for (BestSellingProduct bestSellingProduct: bestSellingProducts) {
-            View cardView = inflater.inflate(R.layout.item_bestselling_product,llBestSellingProducts,false);
-
-            ImageView imgBestSellingProduct = cardView.findViewById(R.id.imgBestSellingProduct);
-            TextView tvBestSellingProductName = cardView.findViewById(R.id.tvBestSellingProductName);
-            TextView tvBestSellingProductPrice = cardView.findViewById(R.id.tvBestSellingProductPrice);
-
-            imgBestSellingProduct.setImageResource(bestSellingProduct.getImg());
-            tvBestSellingProductName.setText(bestSellingProduct.getTitle());
-            //tvBestSellingProductPrice.setText(" €" + bestSellingProduct.getPrice()); //conversão do preço para string
-
-            llBestSellingProducts.addView(cardView);
-
-        }
-   
-         */
 
         return view;
     }
